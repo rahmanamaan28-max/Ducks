440ea7bb219b71528c9865a0618493ca6d682217
rahmanamaan28-max <rahmanamaan28@gmail.com>
2025-11-14 15:41:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const socketIO = require('socket.io');
-const { createRoom, joinRoom, leaveRoom, startGame, submitWord, submitAnswer, nextRound } = require('./rooms');
+const rooms = require('./rooms'); // Must be named exactly as the file
 
 const app = express();
 app.use(cors());
@@ -12,47 +12,43 @@ const PORT = 4000;
 
 io.on('connection', (socket) => {
   socket.on('createRoom', (username, cb) => {
-    const roomCode = createRoom(username, socket.id);
+    const roomCode = rooms.createRoom(username, socket.id);
     socket.join(roomCode);
     cb(roomCode);
-    io.to(roomCode).emit('roomUpdate', joinRoom(roomCode));
+    io.to(roomCode).emit('roomUpdate', rooms.joinRoom(roomCode));
   });
 
   socket.on('joinRoom', ({ username, roomCode }, cb) => {
-    const res = joinRoom(roomCode, username, socket.id);
+    const res = rooms.joinRoom(roomCode, username, socket.id);
     if (res.success) {
       socket.join(roomCode);
       cb({ success: true });
-      io.to(roomCode).emit('roomUpdate', joinRoom(roomCode));
+      io.to(roomCode).emit('roomUpdate', rooms.joinRoom(roomCode));
     } else {
       cb({ success: false, message: res.message });
     }
   });
 
   socket.on('leaveRoom', ({ roomCode }) => {
-    leaveRoom(roomCode, socket.id);
+    rooms.leaveRoom(roomCode, socket.id);
     socket.leave(roomCode);
-    io.to(roomCode).emit('roomUpdate', joinRoom(roomCode));
+    io.to(roomCode).emit('roomUpdate', rooms.joinRoom(roomCode));
   });
 
   socket.on('startGame', ({ roomCode, mode, rounds }) => {
-    startGame(roomCode, mode, rounds, io);
+    rooms.startGame(roomCode, mode, rounds, io);
   });
 
   socket.on('submitWord', ({ roomCode, word }) => {
-    submitWord(roomCode, socket.id, word, io);
+    rooms.submitWord(roomCode, socket.id, word, io);
   });
 
   socket.on('submitAnswer', ({ roomCode, answer }) => {
-    submitAnswer(roomCode, socket.id, answer, io);
+    rooms.submitAnswer(roomCode, socket.id, answer, io);
   });
 
   socket.on('nextRound', ({ roomCode }) => {
-    nextRound(roomCode, io);
-  });
-
-  socket.on('disconnecting', () => {
-    // Optionally handle disconnect
+    rooms.nextRound(roomCode, io);
   });
 });
 
